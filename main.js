c675e978d4d5084d4add9fe9de60fad9ae9d7b63
1-nty <46821853+1-nty@users.noreply.github.com>
2019-03-05 12:10:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-let i = 0;
-function burning() {}
-function change() {
-  console.log("This is running:");
-  window.setInterval(burning(), 10000);
-  if (i % 2 === 1) {
-    document.getElementById("header1").innerHTML = "Hello?";
-  } else if (i % 2 === 0) {
-    document.getElementById("header1").innerHTML = "Page";
-  } else {
-    document.getElementById("header1").innerHTML = "Does this work?";
-  }
-  i++;
-  return i;
-}
 
-window.setInterval(change(), 3000);
+//def vars
+let i = 0;
+let arr = ["Hello?","Page"]
+// function to run defined
+function change() {
+  document.getElementById("header1").innerHTML = arr[i%2]  
+  i++;
+}
+// Set an incrementer every three seconds
+window.setInterval(function(){change()},3000)
+
